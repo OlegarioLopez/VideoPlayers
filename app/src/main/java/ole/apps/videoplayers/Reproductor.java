139fef541bd5e8f88e0fa2f9024693beea29cd4e
Olegario Lopez Linares <olopezl@ayesa.es>
2020-04-08 12:00:06 +0200

    first commit

---
+++ app/src/main/java/ole/apps/videoplayers/Reproductor.java
@@ -0,0 +1,14 @@
+package ole.apps.videoplayers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reproductor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reproductor);
+    }
+}
